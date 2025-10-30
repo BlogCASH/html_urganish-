1bb0b8ef9ff67e37f5ceaf5da184a5fb640794fe
BlogCASH <playzone937@gmail.com>
2025-10-30 19:22:57 +0500

    kvadrat yuzasini topish

--- rekursiv/misol.py
+++ rekursiv/misol.py
@@ -1,22 +1,5 @@
-class Talaba:
-#     """Talaba nomli klass yaratamiz"""
-#     def __init__(self,ism,familiya,tyil):
-#         """Talabaning xususiyatlari"""
-#         self.ism = ism
-#         self.familiya = familiya
-#         self.tyil = tyil
-    
-#     def get_name(self):
-#         """Talabaning ismini qaytaradi"""
-#         return self.ism
-    
-#     def get_lastname(self):
-#         """Talabaning familiyasini qaytaradi"""
-#         return self.familiya
-    
-#     def get_fullname(self):
-#         """Talabaning ism-familiyasini qaytaradi"""
-#         return f"{self.ism} {self.familiya}"
-    
-#     def tanishtir(self):
-#         print(f"Ismim {self.ism} {self.familiya}. {self.tyil} yilda tu'gilganman")
+# Kvadratning yuzini hisoblaymiz
+kvdrt_tmni = 20 # Kavdratning tomoni 20 ga teng
+kvdrt_yuzi = kvdrt_tmni**2 # Kvadrat yuzini hisoblaymiz
+print(kvdrt_yuzi)
+ 
