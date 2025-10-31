2d0c28beb73064d54b135a887308dad54413d29b
BlogCASH <playzone937@gmail.com>
2025-10-31 17:38:42 +0500

    royhatda uzunlik sanash

--- yashil.py
+++ yashil.py
@@ -1,4 +1,6 @@
-# mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
-# narhlar = [12000, 18000, 10900, 22000] # narhlar ro'yxati (sonlar)
-# sonlar = ['bir', 'ikki', 3, 4, 5] # sonlar va matnlar aralash ro'yxat
-# ismlar = [] # bo'sh ro'yxat
+# # mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+# # narhlar = [12000, 18000, 10900, 22000] # narhlar ro'yxati (sonlar)
+# # sonlar = ['bir', 'ikki', 3, 4, 5] # sonlar va matnlar aralash ro'yxat
+# # ismlar = [] # bo'sh ro'yxat
+# fruits = ['pear','banana','apple','watermelon','lemon']
+# print("Elementlar soni:",len(fruits)) # len(fruits) ro'yxat uzunligini qaytaradi
