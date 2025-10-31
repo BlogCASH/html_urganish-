39adaf32dc0bf3d7bc6d3554c2a91bb7d4877f7a
BlogCASH <playzone937@gmail.com>
2025-10-31 17:36:58 +0500

    royhat bilan ishlash

---
+++ yashil.py
@@ -0,0 +1,4 @@
+# mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+# narhlar = [12000, 18000, 10900, 22000] # narhlar ro'yxati (sonlar)
+# sonlar = ['bir', 'ikki', 3, 4, 5] # sonlar va matnlar aralash ro'yxat
+# ismlar = [] # bo'sh ro'yxat
