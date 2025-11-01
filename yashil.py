8a9d207a5cb7094dcaf21ebc8cc647f9ee404d40
BlogCASH <playzone937@gmail.com>
2025-11-01 14:48:44 +0500

    misol

--- yashil.py
+++ yashil.py
@@ -1,13 +1,25 @@
-# # # mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
-# # # narhlar = [12000, 18000, 10900, 22000] # narhlar ro'yxati (sonlar)
-# # # sonlar = ['bir', 'ikki', 3, 4, 5] # sonlar va matnlar aralash ro'yxat
-# # # ismlar = [] # bo'sh ro'yxat
-# # fruits = ['pear','banana','apple','watermelon','lemon']
-# # print("Elementlar soni:",len(fruits)) # len(fruits) ro'yxat uzunligini qaytaradi
-# def getArea(r,pi=3.14159):
-#     """Doiraning yuzini qaytaruvchi funksiya"""
-#     return pi*(r**2)
+# # # # mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+# # # # narhlar = [12000, 18000, 10900, 22000] # narhlar ro'yxati (sonlar)
+# # # # sonlar = ['bir', 'ikki', 3, 4, 5] # sonlar va matnlar aralash ro'yxat
+# # # # ismlar = [] # bo'sh ro'yxat
+# # # fruits = ['pear','banana','apple','watermelon','lemon']
+# # # print("Elementlar soni:",len(fruits)) # len(fruits) ro'yxat uzunligini qaytaradi
+# # def getArea(r,pi=3.14159):
+# #     """Doiraning yuzini qaytaruvchi funksiya"""
+# #     return pi*(r**2)
 
-# def getPerimeter(r,pi=3.14159):
-#     """Doiraning perimetrini qaytaruvchi funksiya"""
-#     # return 2*pi*r
+# # def getPerimeter(r,pi=3.14159):
+# #     """Doiraning perimetrini qaytaruvchi funksiya"""
+# #     # return 2*pi*r
+# import unittest
+# from circle import getArea, getPerimeter
+
+# class CircleTest(unittest.TestCase):
+#     def test_area(self):
+#         self.assertAlmostEqual(getArea(10), 314.159)
+#         self.assertAlmostEqual(getArea(3),28.27431)
+#     def test_perimeter(self):
+#         self.assertAlmostEqual(getPerimeter(10), 62.8318)
+#         self.assertAlmostEqual(getPerimeter(4), 25.13272)
+        
+# unittest.main())
