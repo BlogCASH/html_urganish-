d1fbed37ad2504989feaaee98197ad870b9b6fd8
BlogCASH <playzone937@gmail.com>
2025-11-01 14:47:06 +0500

    sonlarni tekshirish

--- yashil.py
+++ yashil.py
@@ -1,6 +1,13 @@
-# # mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
-# # narhlar = [12000, 18000, 10900, 22000] # narhlar ro'yxati (sonlar)
-# # sonlar = ['bir', 'ikki', 3, 4, 5] # sonlar va matnlar aralash ro'yxat
-# # ismlar = [] # bo'sh ro'yxat
-# fruits = ['pear','banana','apple','watermelon','lemon']
-# print("Elementlar soni:",len(fruits)) # len(fruits) ro'yxat uzunligini qaytaradi
+# # # mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+# # # narhlar = [12000, 18000, 10900, 22000] # narhlar ro'yxati (sonlar)
+# # # sonlar = ['bir', 'ikki', 3, 4, 5] # sonlar va matnlar aralash ro'yxat
+# # # ismlar = [] # bo'sh ro'yxat
+# # fruits = ['pear','banana','apple','watermelon','lemon']
+# # print("Elementlar soni:",len(fruits)) # len(fruits) ro'yxat uzunligini qaytaradi
+# def getArea(r,pi=3.14159):
+#     """Doiraning yuzini qaytaruvchi funksiya"""
+#     return pi*(r**2)
+
+# def getPerimeter(r,pi=3.14159):
+#     """Doiraning perimetrini qaytaruvchi funksiya"""
+#     # return 2*pi*r
