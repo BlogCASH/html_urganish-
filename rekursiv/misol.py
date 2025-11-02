d2c33f8d04f24cd11c82f62fe631eed92d29079f
BlogCASH <playzone937@gmail.com>
2025-11-02 10:54:45 +0500

    sonning yig'indisin topish

--- rekursiv/misol.py
+++ rekursiv/misol.py
@@ -1,14 +1,20 @@
-# def darajaga_kotar(son, daraja):
-#     """Sonni darajaga ko'taruvchi rekursiv funksiya."""
-#     if daraja == 0:
-#         return 1
+# # def darajaga_kotar(son, daraja):
+# #     """Sonni darajaga ko'taruvchi rekursiv funksiya."""
+# #     if daraja == 0:
+# #         return 1
+# #     else:
+# #         return son * darajaga_kotar(son, daraja - 1)
+# #     #  return son ** daraja
+# # print(darajaga_kotar(2, 10))
+# def parametrni_top(son):
+#     """Berilgan sonning raqamlar yig'indisini topuvchi rekursiv funksiya."""
+#     if son == 0:
+#         return 0
 #     else:
-#         return son * darajaga_kotar(son, daraja - 1)
-#     #  return son ** daraja
-# print(darajaga_kotar(2, 10))
-def parametrni_top(son):
-    """Berilgan sonning raqamlar yig'indisini topuvchi rekursiv funksiya."""
-    if son == 0:
-        return 0
+#         return son % 10 + parametrni_top(son // 10)
+def sonning_yigindisini_top(son ,sonlar):
+    if son + sonlar:
+        return son + sonlar
     else:
-        return son % 10 + parametrni_top(son // 10)
+        return sonning_yigindisini_top(son // 10, sonlar + son % 10)
+print(sonning_yigindisini_top(1234,0))
