40c9ed18d96de306d2d4cd4e0883cf50a3d2bf77
BlogCASH <playzone937@gmail.com>
2025-11-02 10:50:39 +0500

    Berilgan sonning raqamlar yig'indisini topuvchi rekursiv funksiya.

--- rekursiv/misol.py
+++ rekursiv/misol.py
@@ -6,3 +6,9 @@
 #         return son * darajaga_kotar(son, daraja - 1)
 #     #  return son ** daraja
 # print(darajaga_kotar(2, 10))
+def parametrni_top(son):
+    """Berilgan sonning raqamlar yig'indisini topuvchi rekursiv funksiya."""
+    if son == 0:
+        return 0
+    else:
+        return son % 10 + parametrni_top(son // 10)
