353bb7334434e6ac680f7a0f0935e47ceb814cec
BlogCASH <playzone937@gmail.com>
2025-11-03 19:11:55 +0500

    misollarim

--- rekursiv/misol.py
+++ rekursiv/misol.py
@@ -2,3 +2,5 @@
 # pi = 3.14159
 # aylana_yuzi = pi * radius**2
 # print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+# ism = input("Ismingiz nima?\n>>>") # Foydalanuvchi ismini yangi qatordan kiritadi
+# print("Assalom alaykum, " + ism.title())
